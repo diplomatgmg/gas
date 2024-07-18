8b8db35f2b6b1f1640315274fcbc266e60d5aa88
Кирилл Попов <diplomatgmg@gmail.com>
2024-07-18 21:17:47 +0300

    Вынесена логика обработки транзакции в отдельную функцию

--- gas_station_system.py
+++ gas_station_system.py
@@ -13,6 +13,7 @@ from base import (
 
 class GasStationSystem(BaseSystem):
     base_url: str = "https://test-app.avtoversant.ru"
+    stations: dict[str, dict] = {}
 
     def auth(self, credential: Credential) -> None:
         self.base_url = credential.url or self.base_url
@@ -35,7 +36,7 @@ class GasStationSystem(BaseSystem):
 
         super().auth(credential)
 
-    def get_stations(self) -> dict[str, dict]:
+    def save_stations(self) -> None:
         """
         Получает все станции и возвращает их в виде словаря
         Ключ - id станции
@@ -44,13 +45,12 @@ class GasStationSystem(BaseSystem):
         stations_url = f"{self.base_url}/abakam/gasstations/stations"
         response = self._get_response("get", stations_url)
         stations = response.json()
-        stations_dict = {str(station["id"]): station for station in stations}
-        return stations_dict
+        self.stations = {str(station["id"]): station for station in stations}
 
     def get_transactions(
         self, from_date: datetime, to_date: datetime
     ) -> list[Transaction]:
-        stations = self.get_stations()
+        self.save_stations()
         transactions: list[Transaction] = []
         transactions_url = f"{self.base_url}/account/transactions?page_size=100"
 
@@ -63,67 +63,72 @@ class GasStationSystem(BaseSystem):
 
         response = self._get_response("post", transactions_url, json=data)
         soup = BeautifulSoup(response.text, "html.parser")
-
-        page_elements = soup.find_all("li", {"class": "page-item"})
-        # Если транзакций за выбранный период нет, то не будет пагинации
-        if not page_elements:
-            count_pages = 1
-        else:
-            count_pages = int(page_elements[-2].get_text(strip=True))
+        count_pages = self._extract_page_count(soup)
 
         for page in range(1, count_pages + 1):
             data["page"] = page
 
             response = self._get_response("post", transactions_url, json=data)
             soup = BeautifulSoup(response.text, "html.parser")
-
             rows = soup.find_all("tr")
 
             for row in rows:
                 if "Пополнение" in row.text:
                     continue
 
-                (
-                    transaction_id,
-                    date_str,
-                    contract,
-                    card,
-                    station_code,
-                    service,
-                    volume,
-                    transaction_sum,
-                ) = (td.get_text(strip=True) for td in row.find_all("td"))
-
-                contracts = self.credential.contracts
-                if contracts and contract not in contracts:
-                    continue
-
-                station_dict = stations.get(station_code)
-
-                if not station_dict:
-                    # Станция может быть удалена и не отображаться на карте leaflet
-                    continue
-
-                point = Point(lat=station_dict.get("lat"), lng=station_dict.get("lng"))
-                station = Station(
-                    code=station_code,
-                    name=station_dict.get("name"),
-                    brand=station_dict.get("brand"),
-                    point=point,
-                    address=station_dict.get("address"),
-                )
-
-                transaction = Transaction(
-                    credential=self.credential,
-                    station=station,
-                    card=card,
-                    code=transaction_id,
-                    date=datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S"),
-                    service=service,
-                    sum=float(transaction_sum),
-                    volume=float(volume),
-                )
-
-                transactions.append(transaction)
+                transaction = self._parse_transaction_row(row)
+                if transaction:
+                    transactions.append(transaction)
 
         return transactions
+
+    @staticmethod
+    def _extract_page_count(soup: BeautifulSoup) -> int:
+        page_elements = soup.find_all("li", {"class": "page-item"})
+        # Если транзакций за выбранный период нет, то не будет пагинации
+        if not page_elements:
+            return 1
+        return int(page_elements[-2].get_text(strip=True))
+
+    def _parse_transaction_row(self, row: BeautifulSoup) -> Transaction | None:
+        (
+            transaction_id,
+            date_str,
+            contract,
+            card,
+            station_code,
+            service,
+            volume,
+            transaction_sum,
+        ) = (td.get_text(strip=True) for td in row.find_all("td"))
+
+        contracts = self.credential.contracts
+        if contracts and contract not in contracts:
+            # Если транзакция не подходит под выбранные контракты
+            return None
+
+        station_dict = self.stations.get(station_code)
+        if not station_dict:
+            # Станция может быть удалена и не отображаться на карте leaflet
+            return None
+
+        point = Point(lat=station_dict.get("lat"), lng=station_dict.get("lng"))
+
+        station = Station(
+            code=station_code,
+            point=point,
+            name=station_dict.get("name"),
+            brand=station_dict.get("brand"),
+            address=station_dict.get("address"),
+        )
+
+        return Transaction(
+            credential=self.credential,
+            station=station,
+            card=card,
+            code=transaction_id,
+            date=datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S"),
+            service=service,
+            sum=float(transaction_sum),
+            volume=float(volume),
+        )
